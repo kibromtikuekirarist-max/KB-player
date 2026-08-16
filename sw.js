0c9c934bfaf5f98e02b8ebb13785cb7bc5ab4d72
kibromtikuekirarist-max <kibromtikuekirarist@gmail.com>
2026-08-16 13:29:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => {
-    return response || fetch(e.request);
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('kb-player').then((cache) => {
+    return cache.addAll(['./index.html', './manifest.json']);
   }));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
