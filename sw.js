fc135085fa3bc0d6272cae102e3c8097a0e82773
kibromtikuekirarist-max <kibromtikuekirarist@gmail.com>
2026-08-17 13:18:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kb-player-pro-v1';
+const CACHE_NAME = 'kb-player-pro-v2';
 const ASSETS_TO_CACHE = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
