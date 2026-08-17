e02d02b9cccef1a8b6685f9ac77fe68908e5f994
kibromtikuekirarist-max <kibromtikuekirarist@gmail.com>
2026-08-17 12:49:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,14 @@
 const CACHE_NAME = 'kb-player-pro-v1';
-const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.json'
-];
+const ASSETS_TO_CACHE = ['/', '/index.html', '/manifest.json'];
 
-// Install Service Worker and Cache all core assets
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
     );
 });
 
-// Activate and clean up old caches
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Fetch assets from cache when offline
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
+        caches.match(event.request).then((cachedResponse) => cachedResponse || fetch(event.request))
     );
 });
